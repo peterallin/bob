e05322e4805ef684355567fcfca69e94ad6bc22d
Peter Allin <peter@peca.dk>
2021-09-06 20:09:28 +0200

    Can now build multiple presets in one go

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,17 @@ fn main() -> Result<()> {
         serde_json::from_str(&presets_json).context("Failed to parse input JSON")?;
     let visible_presets: Vec<_> = presets
         .configure_presets
-        .iter()
+        .into_iter()
         .filter(|p| !p.hidden && p.binary_dir.is_some())
         .collect();
     let presets = select_presets(&visible_presets);
     for preset in presets {
-        config_and_build(&preset.name, &preset.binary_dir.as_ref().unwrap(), &work_dir)?;
+        println!("----- {} -----", &preset.display_name.as_ref().unwrap());
+        config_and_build(
+            &preset.name,
+            &preset.binary_dir.as_ref().unwrap(),
+            &work_dir,
+        )?;
     }
     Ok(())
 }
@@ -46,20 +51,22 @@ struct Options {
     presets: std::path::PathBuf,
 }
 
-fn select_presets<'a>(visible_presets:  &[&'a Preset]) -> impl Iterator<Item=&'a Preset> {
+fn select_presets(visible_presets: &[Preset]) -> impl Iterator<Item = &Preset> {
     let names: Vec<_> = visible_presets
         .iter()
         .map(|p| p.display_name.as_ref().unwrap_or_else(|| &p.name).as_str())
         .collect();
-    let chosen = dialoguer::Select::with_theme(&dialoguer::theme::ColorfulTheme::default())
+    let chosen = dialoguer::MultiSelect::new()
         .items(&names)
-        .with_prompt("Select preset to build")
+        .with_prompt("Select presets to build (cursor up/down, space, return)")
         .interact()
         .unwrap();
-    let preset = visible_presets[chosen];
-    std::iter::once(preset)
+    visible_presets
+        .into_iter()
+        .enumerate()
+        .filter(move |(i, _)| chosen.contains(i))
+        .map(|(_, preset)| preset)
 }
-
 
 fn config_and_build(preset: &str, binary_dir: &str, work_dir: &std::path::Path) -> Result<()> {
     cmake(&["--preset", preset], work_dir).context("CMake configure step failed")?;
@@ -68,10 +75,17 @@ fn config_and_build(preset: &str, binary_dir: &str, work_dir: &std::path::Path) 
 }
 
 fn cmake(args: &[&str], path: &std::path::Path) -> Result<()> {
-    let mut child = std::process::Command::new("cmake").current_dir(path).args(args).spawn().context("Failed to spawn CMake")?;
+    let mut child = std::process::Command::new("cmake")
+        .current_dir(path)
+        .args(args)
+        .spawn()
+        .context("Failed to spawn CMake")?;
     let exit_status = child.wait().context("Failed executing CMake")?;
     if !exit_status.success() {
-        return Err(anyhow!(format!("CMake failed with exit code{}", exit_status)));
+        return Err(anyhow!(format!(
+            "CMake failed with exit code{}",
+            exit_status
+        )));
     }
     Ok(())
 }
